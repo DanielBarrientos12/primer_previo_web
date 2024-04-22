74a1117143319077217898415de954dfb8eaec9c
DanielBarrientos12 <danielbarrientos15342@outlook.com>
2024-04-22 11:58:59 -0500

    se agrega funcion de promedio y se finaliza el proyecto

--- js/notas.js
+++ js/notas.js
@@ -33,7 +33,7 @@ window.addEventListener('load', function() {
                     n2Cell.textContent = nota.n2;
                     n3Cell.textContent = nota.n3;
                     exCell.textContent = nota.ex;
-                    defCell.textContent = ((parseFloat(nota.n1) + parseFloat(nota.n2) + parseFloat(nota.n3) + parseFloat(nota.ex)) / 4).toFixed(2); // Calcula el promedio
+                    defCell.textContent = ((parseFloat(nota.n1) + parseFloat(nota.n2) + parseFloat(nota.n3) + parseFloat(nota.ex)) / 4).toFixed(2); // calculo de las definitivas
 
                     row.appendChild(asignaturaCell);
                     row.appendChild(creditosCell);
@@ -43,6 +43,8 @@ window.addEventListener('load', function() {
                     row.appendChild(exCell);
                     row.appendChild(defCell);
                     tbody.appendChild(row);
+
+                    document.getElementById('promedio-estudiante').textContent = promedio(nota.n1, nota.n2, nota.n3, nota.ex).toFixed(2); //calculo del promedio
                 });
             } else {
                 console.log('No hay notas disponibles para mostrar.');
@@ -57,4 +59,11 @@ document.querySelector('.btn-logout').addEventListener('click', function() {
     localStorage.clear();
     window.location.href = '../index.html';
 });
-``
+
+function promedio (n1, n2, n3, ex) {
+    return (((parseFloat(n1) + parseFloat(n2) + parseFloat(n3)) / 3) * 0.7) + (parseFloat(ex) * 0.3);
+}
+
+function promedioPonderado (def1, cre1, def2, cre2, def3, cre3, def4, cre4, def5, cre5) {
+    return ((def1 * cre1) + (def2 * cre2) + (def3 * cre3) + (def4 * cre4) + (def5 * cre5)) / (cre1 + cre2 + cre3 + cre4 + cre5);
+}
